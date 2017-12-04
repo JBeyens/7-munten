847aa35e195818381607f2460506adc58902ed5d
JBeyens <jefbeyens@gmail.com>
2017-12-04 19:25:53 +0100

    nationaliteit

--- src/model/Nationaliteit.java
+++ src/model/Nationaliteit.java
@@ -1,5 +1,23 @@
 package model;
 
 public class Nationaliteit {
+	
+	private String nationaliteit;
+	private String taal;
+	
+	
+	public String getNationaliteit() {
+		return nationaliteit;
+	}
+	public void setNationaliteit(String nationaliteit) {
+		this.nationaliteit = nationaliteit;
+	}
+	
+	public String getTaal() {
+		return taal;
+	}
+	public void setTaal(String taal) {
+		this.taal = taal;
+	}
 
 }
