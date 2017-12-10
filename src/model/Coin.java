49acacbc1d930b69f5acc21d3bcc3864aea727fd
JBeyens <jefbeyens@gmail.com>
2017-12-10 22:24:21 +0100

    Added  coin object

---
+++ src/model/Coin.java
@@ -0,0 +1,21 @@
+package model;
+
+public class Coin {
+	// MEMBERS
+	String name;
+	double valuePerDefaultCoin;
+	
+	
+	// METHODS
+	/* Getter & Setter for 'name' */
+	public String getName() {
+		return name; }
+	public void setCoin(String name) {
+		this.name = name; }
+
+	/* Getter & Setter for 'valuePerDefaultCoin' */
+	public double getValuePerDefaultCoin() {
+		return valuePerDefaultCoin;	}
+	public void setValuePerDefaultCoin(double valuePerDefaultCoin) {
+		this.valuePerDefaultCoin = valuePerDefaultCoin;	}
+}
