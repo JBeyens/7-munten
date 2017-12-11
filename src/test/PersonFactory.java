25442554fe90dac438ee30e331ef2f34acfc3ef6
JBeyens <jefbeyens@gmail.com>
2017-12-11 23:21:59 +0100

    bit of factory

--- src/test/PersonFactory.java
+++ src/test/PersonFactory.java
@@ -1,6 +1,12 @@
 package test;
 
+import model.properties.CoinLoader;
+
 public class PersonFactory {
+	CoinLoader coinLoader;
 	
+	public PersonFactory() {
+		coinLoader = CoinLoader.getInstance();		
+	}
 	
 }
