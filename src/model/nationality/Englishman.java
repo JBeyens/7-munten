851a4fa9cdc1b85a2625c2056ec6487c3c46f638
JBeyens <jefbeyens@gmail.com>
2017-12-16 00:17:54 +0100

    Englishman

---
+++ src/model/nationality/Englishman.java
@@ -0,0 +1,17 @@
+package model.nationality;
+
+import model.Coin;
+import model.Person;
+
+public class Englishman extends Nationality {
+	// CONSTRUCTOR
+	public Englishman(Coin englandCoin){
+		super(englandCoin);
+	}
+
+	// METHODS
+	@Override
+	public String toString(Person person) {
+		return "Hey, I'm an Englishman and after a cup of tea, my wealth is " + super.toString(person) + ".";
+	}
+}
