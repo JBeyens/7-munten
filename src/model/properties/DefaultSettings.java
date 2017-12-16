0d9206633e8540ec67676f264d5047b333239864
JBeyens <jefbeyens@gmail.com>
2017-12-16 16:59:04 +0100

    small change

--- src/model/properties/DefaultSettings.java
+++ src/model/properties/DefaultSettings.java
@@ -17,7 +17,7 @@ public final class DefaultSettings {
 	public static final String DEFAULTCOIN_NAME = "defaultCoin";
 	public static final String DEFAULTCOIN_ADVISE = "\r\n# '" + DEFAULTCOIN_NAME + "' must be set mandatory\r\n" 
 													+ "# Example: '" + DEFAULTCOIN_NAME + " = eur'";
-	public static final String OTHER_COINS_ADVISE = "\r\n# For other coins, the value which corresponds to 1 "+ DEFAULTCOIN_NAME +" must be given"
+	public static final String OTHER_COINS_ADVISE = "\r\n# For other coins, the value which corresponds to 1 "+ DEFAULTCOIN_NAME + " must be given"
 			+ "\r\n# Example: 'usd=1.19' => If " + DEFAULTCOIN_NAME + " is in Euro, then this means '1 Euro = 1.19 US dollars'";
 	public static final String OTHER_COIN = "USD";
 	public static final String OTHER_COIN_VALUE = "1.19";
