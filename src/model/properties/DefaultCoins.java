f9e85b00c70377da6b568f0f29497eff1329b301
JBeyens <jefbeyens@gmail.com>
2017-12-18 19:34:36 +0100

    default coin values seperated

---
+++ src/model/properties/DefaultCoins.java
@@ -0,0 +1,37 @@
+package model.properties;
+/**
+ * 	@Author Jef Beyens & Ben Vandevorst
+	@Datum 09/10/2017
+	@Project Calculator
+	@Doel Holding default coin values
+ */
+public enum DefaultCoins
+{
+	defaultCoin("btc"),
+	usd("1.31774"),
+	gbp("0.84306"),
+	nok("8.0157"),
+	sek("8.7332"),
+	chf("1.2357");
+		
+	private final String name;
+		
+	DefaultCoins(String name)
+	{
+		this.name = name;
+	}
+		
+	public String getValue()
+	{
+		return name;
+	}
+		
+	@Override
+	public String toString()
+	{
+		return this.name();
+	}
+		
+	
+
+}
--- src/model/properties/DefaultSettings.java
+++ src/model/properties/DefaultSettings.java
@@ -11,16 +11,13 @@ import org.apache.log4j.Level;
 
 public final class DefaultSettings {
 	public static final Level LOGGING_LEVEL = Level.ALL;
-	
 	public static final String PROPERTIES_PATH = "bin/model/properties/coins.properties";
-	public static final String DEFAULTCOIN = "EUR";
+
 	public static final String DEFAULTCOIN_NAME = "defaultCoin";
-	public static final String DEFAULTCOIN_ADVISE = "\r\n# '" + DEFAULTCOIN_NAME + "' must be set mandatory\r\n" 
-													+ "# Example: '" + DEFAULTCOIN_NAME + " = eur'";
-	public static final String OTHER_COINS_ADVISE = "\r\n# For other coins, the value which corresponds to 1 "+ DEFAULTCOIN_NAME + " must be given"
-			+ "\r\n# Example: 'usd=1.19' => If " + DEFAULTCOIN_NAME + " is in Euro, then this means '1 Euro = 1.19 US dollars'";
-	public static final String OTHER_COIN1 = "USD";
-	public static final String OTHER_COIN1_VALUE = "1.19";
+	public static final String DEFAULTCOIN_ADVISE = "\r\n# 'defaultCoin' must be set mandatory\r\n # Example: 'defaultCoin = eur'";
+	public static final String OTHER_COINS_ADVISE = "\r\n# For other coins, the value which corresponds to 1 defaultCoin must be given"
+			+ "\r\n# Example: 'usd=1.19' => If defaultCoin is in Euro, then this means '1 Euro = 1.19 US dollars'";
+	
 	public static final double OTHER_COINS_DEFAULT_VALUE = 1.0;
 	
 }
